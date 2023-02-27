6e6cd9ab609a4012a7002c48bb0a51802e6b16c7
Wesley Peters <wespeters18@gmail.com>
2023-02-27 10:32:04 -0500

    something does seem wrong with gravity when adding rotation

--- app/basic_scene.h
+++ app/basic_scene.h
@@ -22,13 +22,19 @@ return_code app_init()
     dm_ecs_entity_add_transform(entities[entity_count], 0,4,0, 0,0,0, 0,0,0,1);
     add_point_light_component(entities[entity_count], dm_vec4_set(1,1,1,1), dm_vec4_set(1,1,1,1), dm_vec4_set(1,1,1,1), dm_vec3_set(0,0,0), 1,0.0009f,0.00007f);
     
-#define PLANE_SCALE 10.0f
-#define PLANE_DEPTH  0.1f
     entities[++entity_count] = dm_ecs_create_entity();
+#define PLANE_SCALE 10.0f
+#ifndef OBJ_SPHERE
+#define PLANE_DEPTH  0.1f
     dm_ecs_entity_add_transform(entities[entity_count], 0,0,0, PLANE_SCALE,PLANE_DEPTH,PLANE_SCALE, 0,0,0,1);
-    dm_ecs_entity_add_collision_box(entities[entity_count], dm_vec3_set(-PLANE_SCALE * 0.5f,-0.05f,-PLANE_SCALE * 0.5f), dm_vec3_set(PLANE_SCALE * 0.5f,PLANE_DEPTH * 0.5f,PLANE_SCALE * 0.5f));
-    dm_ecs_entity_add_physics_at_rest(entities[entity_count], 1e13f, DM_PHYSICS_BODY_TYPE_RIGID, DM_PHYSICS_MOVEMENT_KINEMATIC);
+    dm_ecs_entity_add_collision_box(entities[entity_count], dm_vec3_set(-PLANE_SCALE * 0.5f,-PLANE_DEPTH * 0.5f,-PLANE_SCALE * 0.5f), dm_vec3_set(PLANE_SCALE * 0.5f,PLANE_DEPTH * 0.5f,PLANE_SCALE * 0.5f));
     dm_ecs_entity_add_mesh(entities[entity_count], BOX_MESH);
+#else
+    dm_ecs_entity_add_transform(entities[entity_count], 0,0,0, PLANE_SCALE * 0.5f,PLANE_SCALE * 0.5f,PLANE_SCALE * 0.5f, 0,0,0,1);
+    dm_ecs_entity_add_collision_sphere(entities[entity_count], PLANE_SCALE * 0.5f);
+    dm_ecs_entity_add_mesh(entities[entity_count], ICOSPHERE_MESH);
+#endif
+    dm_ecs_entity_add_physics_at_rest(entities[entity_count], 1e13f, DM_PHYSICS_BODY_TYPE_RIGID, DM_PHYSICS_MOVEMENT_KINEMATIC);
     dm_ecs_entity_add_material(entities[entity_count], dm_vec4_set(0.5f,0.15f,0,1), dm_vec4_set(0.5f,0,0,1));
     
     float size = 1.0f;
@@ -40,7 +46,7 @@ return_code app_init()
     dm_ecs_entity_add_collision_box(entities[entity_count], dm_vec3_set(-half_s, -half_s, -half_s), dm_vec3_set(half_s, half_s, half_s));
     dm_ecs_entity_add_mesh(entities[entity_count], BOX_MESH);
 #else
-    dm_ecs_entity_add_transform(entities[entity_count], 0,5,0, half_s,half_s,half_s, 1,0.25f,1,1);
+    dm_ecs_entity_add_transform(entities[entity_count], 0,15,0, half_s,half_s,half_s, 1,0.25f,1,1);
     dm_ecs_entity_add_collision_sphere(entities[entity_count], half_s);
     dm_ecs_entity_add_mesh(entities[entity_count], ICOSPHERE_MESH);
 #endif
