48ccad9f617a9da72376caefc645bb961a3559cd
Wesley Peters <wespeters18@gmail.com>
2023-08-23 13:34:45 -0400

    initial commit

---
+++ assets/shaders/debug_pixel.glsl
@@ -0,0 +1,15 @@
+#version 460 core
+
+struct ps_input
+{
+	vec4 position;
+	vec4 color;
+};
+
+layout(location=0) in ps_input vs_output;
+out vec4 frag_color;
+
+void main()
+{
+	frag_color = vs_output.color;
+}
